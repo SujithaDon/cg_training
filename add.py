e4966aeb6de37be8bcb4914a2db5c63ed3129aca
SujithaDon <32532029+SujithaDon@users.noreply.github.com>
2020-01-22 16:35:20 +0530

    the first ppt of training DWH

---
+++ add.py
@@ -0,0 +1,9 @@
+#to read values from the user
+a=int(input("enter tha value of a"))
+b=int(input("enter the value of b"))
+#adds the values
+c=a+b
+if(c>0):
+    print("positive")
+else:
+    print("negative")
